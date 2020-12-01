6b121e9596bc9ab6c1840bf65163975fa8d929ae
Beno Učakar <beno.ucakar@gmail.com>
2020-12-01 16:07:04 +0100

    Dodal template.

---
+++ template.ml
@@ -0,0 +1,23 @@
+let naloga1 vsebina_datoteke =
+    None
+
+let naloga2 vsebina_datoteke =
+    None
+
+let _ =
+    let preberi_datoteko ime_datoteke =
+        let chan = open_in ime_datoteke in
+        let vsebina = really_input_string chan (in_channel_length chan) in
+        close_in chan;
+        vsebina
+    and izpisi_datoteko ime_datoteke vsebina =
+        let chan = open_out ime_datoteke in
+        output_string chan vsebina;
+        close_out chan
+    in
+    let vsebina_datoteke = preberi_datoteko "day_0.in" in
+    let odgovor1 = naloga1 vsebina_datoteke
+    and odgovor2 = naloga2 vsebina_datoteke
+    in
+    izpisi_datoteko "day_0_1.out" odgovor1;
+    izpisi_datoteko "day_0_2.out" odgovor2
